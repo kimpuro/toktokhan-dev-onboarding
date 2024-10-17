f03d367647481a0dfdb266e81ffc34e9b450ff97
kimpuro <puro@protonmail.ch>
2024-10-17 15:26:12 +0900

    T-21

--- next-page-init/src/containers/Home/Home.tsx
+++ next-page-init/src/containers/Home/Home.tsx
@@ -1,21 +1,49 @@
-import { Center, Text } from '@chakra-ui/react'
+import {Card, Center, Text} from '@chakra-ui/react'
 import TemplateLayout from "@/components/@Templates/TemplateLayout";
+import {InfinityList} from "@toktokhan-dev/chakra";
+import {usePostsControllerFindAllInfiniteQuery} from "@/swagger/Posts/Posts.query";
+import {useMemo} from "react";
 
 function Home() {
+  const { data, hasNextPage, isFetching, fetchNextPage } =
+      usePostsControllerFindAllInfiniteQuery({
+        variables: {
+          query: {
+            limit: 5,
+          },
+        },
+        options: {
+          select: (data) => data.pages.flatMap((page) => page.result || []), // 1차원 배열로 변경
+        },
+      })
+
+  const posts = useMemo(() => data ?? [], [data])
   return (
       <TemplateLayout title={"Home"}>
-    <Center h={'100%'} borderRadius={'8px'} flexDir={'column'} gap={'20px'}>
-      <Text
-        color={'primary.3'}
-        textStyle={'pre-heading-01'}
-        textAlign={'center'}
-      >
-        TOKTOKHAN.DEV
-      </Text>
-      <Text color={'content.1'} textStyle={'pre-heading-02'}>
-        Next page template
-      </Text>
-    </Center>
+        <InfinityList
+            data={posts}
+            hasMore={hasNextPage}
+            isFetching={isFetching}
+            onFetchMore={fetchNextPage}
+            renderItem={(post) => (
+                <Card
+                    key={post.id}
+                    p="25px"
+                    gap="10px"
+                    borderRadius="16px"
+                    w="100%"
+                    h="144px"
+                    bg="background.primary"
+                    direction="column"
+                    mb="20px"
+                >
+                  <Text textStyle="pre-heading-03">{post.title}</Text>
+                  <Text textStyle="pre-body-03" color="text.secondary">
+                    {post.content}
+                  </Text>
+                </Card>
+            )}
+        />
       </TemplateLayout>
   )
 }
---
+++ next-page-init/src/test.js
@@ -0,0 +1,2 @@
+const 한글 = '한글';
+console.log(한글);
